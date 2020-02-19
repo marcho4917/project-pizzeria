bd6c5ce2535055f4efd0cf99eeb6b7c543065ff5
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-02-19 11:10:38 +0100

    fix default value, zadanie_11.1 API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -41,28 +41,56 @@ class Booking {
   getData() {
     const thisBooking = this;
 
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
     const params = {
       booking: [
-        settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate)
+        startDateParam,
+        endDateParam,
       ],
       eventsCurrent: [
-
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
       ],
       eventsRepeat: [
-
+        settings.db.repeatParam,
+        endDateParam,
       ],
     };
 
-    console.log('getData params', params);
+    //console.log('getData params', params);
 
-    /*const urls = {
+    const urls = {
       booking:       settings.db.url + '/' + settings.db.booking 
                                      + '?' + params.booking.join('&'),
       eventsCurrent: settings.db.url + '/' + settings.db.event   
                                      + '?' + params.eventsCurrent.join('&'),
       eventsRepeat:  settings.db.url + '/' + settings.db.event   
                                      + '?' + params.eventsRepeat.join('&'),
-    };*/
+    };
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 }
 
