49de73376f2806eb3b37c9c4b20d9a96250dd8bf
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-02-29 16:07:58 +0100

    Fix carousel in app

--- src/js/app.js
+++ src/js/app.js
@@ -93,6 +93,7 @@ const app = {
     thisApp.initData();
     thisApp.initCart();
     thisApp.initBooking();
+    thisApp.initCarousel();
   },
   initCart: function () {
     const thisApp = this;
@@ -113,30 +114,32 @@ const app = {
 
     thisApp.booking = new Booking(bookingContainer);
   },
-  initCarousel: function () {
+  initCarousel() {
     let slideIndex = 0;
 
-    const slides = document.querySelectorAll('.slide');
-    const dots = document.querySelectorAll('.carousel-dot');
-    //console.log(slides);
-    //console.log(dots);
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].classList.remove('active');
-    }
-    slideIndex++;
-    //console.log(slideIndex);
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    //console.log(slideIndex);
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].classList.remove('active');
-    }
-    slides[slideIndex - 1].classList.add('active');
-    dots[slideIndex - 1].classList.add('active');
+    function showSlides() {
+      const slides = document.querySelectorAll('.slide');
+      const dots = document.querySelectorAll('.carousel-dot');
+      //console.log(slides);
+      //console.log(dots);
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+      }
+      slideIndex++;
+      //console.log(slideIndex);
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      //console.log(slideIndex);
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+      }
+      slides[slideIndex - 1].classList.add('active');
+      dots[slideIndex - 1].classList.add('active');
 
-    //setTimeout(showSlides, 3000);
-
+      setTimeout(showSlides, 3000);
+    } 
+    showSlides();
   }
 };
 app.init();
